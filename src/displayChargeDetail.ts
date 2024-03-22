8def92599b69287f0660cd435378298f80ccd2b4
takagakiryuheiCM <takagaki.ryuhei@classmethod.jp>
2024-03-22 10:45:02 +0900

    feat: 表示関数作成

---
+++ src/displayChargeDetail.ts
@@ -0,0 +1,18 @@
+import { calcChange } from "./calcChange"
+
+type TDisplayChargeDetail = {
+  price: number;
+  paid: number
+}
+
+// 最終的なアウトプットを出力する関数
+export const displayChargeDetail = ({ price, paid }: TDisplayChargeDetail) => {
+  const change = paid - price
+  const changeResultArray: Change[] = calcChange(change)
+
+  // 最終的なアウトプット
+  console.log(`お釣りは、${change}円です`)
+  changeResultArray.forEach((value: Change) => {
+    cosole.log(value.displayDetail())
+  })
+}
