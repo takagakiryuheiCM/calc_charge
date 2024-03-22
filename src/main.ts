10a23d036988d93eb2f1c4de6bb166d5bd84eb90
takagakiryuheiCM <takagaki.ryuhei@classmethod.jp>
2024-03-22 11:06:20 +0900

    fix: 入れる値が間違っていたため修正

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { displayChargeDetail } from "./displayChargeDetail";
 
 // 課題：https://www.notion.so/2f74984a49554d5f8c985d132b9ea9d5
-displayChargeDetail({ price: 500, paid: 345 })
+displayChargeDetail({ price: 345, paid: 500 })
 // => 
 // お釣りは、155円です。
 // 100円硬貨 1枚
