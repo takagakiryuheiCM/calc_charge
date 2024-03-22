43aecc5968a96c2776ab06ba9272a45427c1bd1e
takagakiryuheiCM <takagaki.ryuhei@classmethod.jp>
2024-03-22 10:45:23 +0900

    feat: main関数を作成して表示関数を呼び出す

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { displayChargeDetail } from "./displayChargeDetail";
+
+// 課題：https://www.notion.so/2f74984a49554d5f8c985d132b9ea9d5
+displayChargeDetail({ price: 500, paid: 345 })
+// => 
+// お釣りは、155円です。
+// 100円硬貨 1枚
+// 50円硬貨 1枚
+// 5円硬貨 1枚
